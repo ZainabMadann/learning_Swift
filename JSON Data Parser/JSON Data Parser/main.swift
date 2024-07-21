5f91f2a442405d6a6787373df543a1f230a3dd9b
zabdulla <zainabmadann@gmail.com>
2024-07-21 20:01:14 +0300

    its working

--- JSON Data Parser/JSON Data Parser/main.swift
+++ JSON Data Parser/JSON Data Parser/main.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+print("wlcome to your JSON Data Parser Tool ")
+
+do {
+    print("enter the path of the file you want to Parse ?")
+    let filePath = readLine() ?? ""
+
+    let fileManager = FileManager.default
+
+    if !fileManager.fileExists(atPath: filePath) {
+        print("file is not there !")
+        exit(1)
+    }
+
+    let pathurl = URL(fileURLWithPath: filePath)
+    let fileData = try Data(contentsOf: pathurl)
+
+    let json = try JSONSerialization.jsonObject(with: fileData, options: []) as? [String: Any]
+    if json == nil {
+        print("invaild json !")
+        exit(1)
+    }
+
+    print("what is you want to search about ?")
+    let search = readLine() ?? ""
+
+
+    if let searchResult = json?[search] {
+        print("Found: \(searchResult)")
+    } else {
+        print("Key '\(search)' Not Found.")
+    }
+}catch{
+    print("Error reading or parsing the file: \(error)")
+    exit(1)
+}
 
