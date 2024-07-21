9d0a49e716ab7e3747f5b61c603b1ff43c63f8c0
zabdulla <zainabmadann@gmail.com>
2024-07-21 23:03:59 +0300

    alsmaak yabi 3asha

--- Task Scheduler with Alerts/Task Scheduler with Alerts/main.swift
+++ Task Scheduler with Alerts/Task Scheduler with Alerts/main.swift
@@ -7,3 +7,63 @@
 
 import Foundation
 
+print("welcome to your command-line to-do list manager , enter 1 for adding a task , 2 for marking a task as complete , 3 for display all tasks and 4 for exit ")
+
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+struct Task {
+    var taskName = ""
+    var taskStatus = ""
+    var deadline : Date
+    var isOverdue: Bool {
+        return deadline < Date()
+    }
+}
+
+var list = [Task]()
+while true {
+    let option = Int(readLine() ?? "") ?? 0
+    for i in 0..<list.count {
+        if list[i].isOverdue == true{
+            print("your task : '\(list[i].taskName)' is overdue !!")
+        }
+    }
+    switch (option) {
+    case 1:
+        print("ok enter your task :")
+        let taskName = readLine() ?? ""
+        print("ok enter your deadline(yyyy-MM-dd HH:mm:ss):")
+        let deadlineString = readLine() ?? ""
+        guard let deadline = dateFormatter.date(from: deadlineString) else {
+                print("Invalid date format.")
+                continue
+        }
+        let newTask = Task(taskName: taskName, taskStatus: "Incomplete" ,deadline: deadline)
+        list.append(newTask)
+    case 2:
+        print("enter the task name you want mask as complete")
+        let taskName = readLine() ?? ""
+          var taskFound = false
+          for i in 0..<list.count {
+              if list[i].taskName == taskName {
+                  list[i].taskStatus = "Complete"
+                  taskFound = true
+                  print("Task '\(taskName)' marked as complete.")
+                  break
+              }
+          }
+          if !taskFound {
+              print("Task '\(taskName)' not found.")
+          }
+    case 3:
+        for i in 0..<list.count {
+            print("\(i) task name: '\(list[i].taskName)' task status: '\(list[i].taskStatus)' task deadline: '\(list[i].deadline)'")
+        }
+    case 4:
+        print("ok bye!")
+        exit(0)
+    default:
+        print("you entered wrong option")
+    }
+}
